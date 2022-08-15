3842ae1c81252d75cb3de4e1e0776b811d7c5f20
zhekichaan <eugene.chernitskiy@gmail.com>
2022-08-15 19:13:07 -0400

    1

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,15 +1,12 @@
 export const Contacts = ({contacts, filter, deleteContact}) => {
-  // let selectContact = (e) => {
-  //   deleteContact()
-  // }
-      return (
-        <ul id="contacts">
-          {contacts.filter(contact => contact.name.toLowerCase().includes(filter)).map((contact) => (
-              <li key={contact.id}>
-                <p>{contact.name}: {contact.number}</p>
-                <button onClick={() => deleteContact(contact.id)}>Delete</button>
-              </li>
-          ))}
-        </ul>
-      )
+  return (
+    <ul id="contacts">
+      {contacts.filter(contact => contact.name.toLowerCase().includes(filter)).map((contact) => (
+          <li key={contact.id}>
+            <p>{contact.name}: {contact.number}</p>
+            <button onClick={() => deleteContact(contact.id)}>Delete</button>
+          </li>
+      ))}
+    </ul>
+  )
 }
