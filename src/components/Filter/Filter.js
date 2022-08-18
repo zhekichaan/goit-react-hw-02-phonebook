674d237ecc2b24b59e3a213817a087d5dded7e1b
zhekichaan <eugene.chernitskiy@gmail.com>
2022-08-17 23:03:28 -0400

    1

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,27 +1,23 @@
 import { Box } from "components/Box";
-import { Component } from "react";
 import PropTypes from 'prop-types';
 import { FilterInput } from "./Filter.styled";
 
 
-export class Filter extends Component {
+export const Filter = ({filter, updateFilter}) => {
+    return (
+        <Box mt="20px">
+            <p>Find contacts by name</p>
+            <FilterInput 
+                type="text"
+                name="filter"
+                value={filter}
+                onChange={(e) => {updateFilter(e.target.value)}}
+            />
+        </Box>
+    )
+}
 
-    static propTypes = {
-        filter: PropTypes.string.isRequired,
-        updateFilter: PropTypes.func.isRequired,
-    }
-
-    render() {
-        return (
-            <Box mt="20px">
-                <p>Find contacts by name</p>
-                <FilterInput 
-                    type="text"
-                    name="filter"
-                    value={this.props.filter}
-                    onChange={(e) => {this.props.updateFilter(e.target.value)}}
-                />
-            </Box>
-        )
-    }
+Filter.propTypes = {
+    filter: PropTypes.string.isRequired,
+    updateFilter: PropTypes.func.isRequired,
 }
